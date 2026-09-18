2235cca6a2a29af8970d818cf71d249f796e4810
Abdenasser Farkli <abdenasserfarkli@gmail.com>
2026-09-18 11:47:28 +0100

    add function filtrerParNiveau

--- src/progression.js
+++ src/progression.js
@@ -1,201 +1,201 @@
-import {apprenants} from './data.js'
+import { apprenants } from './data.js'
 
 
 import promptSync from "prompt-sync";
 
 const prompt = promptSync();
 function normaliserNom(nomComplet) {
-    
-    let isspace= false
+
+    let isspace = false
     nomComplet = nomComplet.trim().replace(/[^a-zA-Z ]/g, "").toLowerCase().split("")
     for (let i = 0; i < nomComplet.length; i++) {
-        if (i==0 || isspace == true) {
-            if (nomComplet[i]==" ") {
-                nomComplet.splice(i,1)
+        if (i == 0 || isspace == true) {
+            if (nomComplet[i] == " ") {
+                nomComplet.splice(i, 1)
                 i--
                 isspace = false
-                
-            }else{
-               nomComplet[i]= nomComplet[i].toUpperCase()
-               isspace = false
+
+            } else {
+                nomComplet[i] = nomComplet[i].toUpperCase()
+                isspace = false
 
 
             }
-            
+
         }
-        if (nomComplet[i]==" ") {
-            isspace=true
+        if (nomComplet[i] == " ") {
+            isspace = true
         }
-        
+
     }
-    nomComplet= nomComplet.join("")
+    nomComplet = nomComplet.join("")
     return nomComplet
-    
+
 }
- function validerResultat(jour, Exercices){
- 
- jour=  Number(prompt("Enter Day you Want to add : "));
- while (jour<1 || jour>7) {
-    console.log("Day Invalid Try again day Includ  from 1 to 7")
-    jour=  Number(prompt("Enter Day you Want to add : "));
+function validerResultat(jour, Exercices) {
 
- }
-  Exercices =Number(prompt("Enter Number Exercices You Finshed : "));
-while (Exercices<1 || Exercices>20) {
-    console.log("Exercices Invalid Try again Exercices  Includ  from 1 to 20");
-    Exercices=  Number(prompt("Enter Exercices you Want to add : "));
+    jour = Number(prompt("Enter Day you Want to add : "));
+    while (jour < 1 || jour > 7) {
+        console.log("Day Invalid Try again day Includ  from 1 to 7")
+        jour = Number(prompt("Enter Day you Want to add : "));
 
- }
- 
- return [jour, Exercices];
- 
+    }
+    Exercices = Number(prompt("Enter Number Exercices You Finshed : "));
+    while (Exercices < 1 || Exercices > 20) {
+        console.log("Exercices Invalid Try again Exercices  Includ  from 1 to 20");
+        Exercices = Number(prompt("Enter Exercices you Want to add : "));
 
- }
- function checkid(id) {
-     id =Number(prompt(" Add Id  : "))
-     
+    }
+
+    return [jour, Exercices];
+
+
+}
+function checkid(id) {
+    id = Number(prompt(" Add Id  : "))
+
     for (let i = 0; i < apprenants.length; i++) {
-        if (apprenants[i].id===id) {
+        if (apprenants[i].id === id) {
             console.log("this Id is Used");
-            
-             id =checkid(id)
-            
+
+            id = checkid(id)
+
         }
-        
+
     }
-    return id 
+    return id
 
-    
- }
- function ajouterApprenant(IdUser,FullName,City) {
-     IdUser =Number(checkid())
-     FullName= normaliserNom(  prompt(" Enter Your Name  : ")) 
-     City =  normaliserNom(  prompt(" Enter Name You City  : "))
-   const info =  {
- id :IdUser,
- nomComplet  : FullName ,
- ville  : City,
- resultats: []
- }
 
-    
-    apprenants.push(info) 
+}
+function ajouterApprenant(IdUser, FullName, City) {
+    IdUser = Number(checkid())
+    FullName = normaliserNom(prompt(" Enter Your Name  : "))
+    City = normaliserNom(prompt(" Enter Name You City  : "))
+    const info = {
+        id: IdUser,
+        nomComplet: FullName,
+        ville: City,
+        resultats: []
+    }
 
-    
- }
+
+    apprenants.push(info)
+
+
+}
 function findIndexById(id) {
-      id = Number(prompt("Enter Id  : "))
+    id = Number(prompt("Enter Id  : "))
     for (let i = 0; i < apprenants.length; i++) {
-        if (apprenants[i].id==id) 
-            return [true,i]
-            
-        
-        
+        if (apprenants[i].id == id)
+            return [true, i]
+
+
+
     }
-                return [false,null]
+    return [false, null]
 
 }
 function misajourRusult(IndexId, result) {
     for (let i = 0; i < apprenants.length; i++) {
-        if (apprenants[i].id==apprenants[IndexId].id) {
+        if (apprenants[i].id == apprenants[IndexId].id) {
             for (let j = 0; j < apprenants[i].resultats.length; j++) {
-                if (apprenants[i].resultats[j].jour==result.jour) {
+                if (apprenants[i].resultats[j].jour == result.jour) {
                     return j
                 }
-                
+
             }
-            
+
         }
-        
-    }
-        return -1
 
-    
+    }
+    return -1
+
+
 }
-function enregistrerResultat(){
-    let isvalid , indexid
-     
-    [isvalid,indexid]=findIndexById()
+function enregistrerResultat() {
+    let isvalid, indexid
+
+    [isvalid, indexid] = findIndexById()
     if (isvalid) {
-       
+
         let day, exercices
-    [day, exercices] = validerResultat();
-    
-    
-    let challenge = prompt("Do you do challenge? true/false: ");
-    challenge = challenge.toLowerCase() === "true";
+        [day, exercices] = validerResultat();
 
 
-    const result={
-         jour: day, exercicesTermines: exercices,
-          totalExercices: 20, challengeTermine: challenge
+        let challenge = prompt("Do you do challenge? true/false: ");
+        challenge = challenge.toLowerCase() === "true";
 
-    }
-    let indexdy = misajourRusult(indexid,result)
-    if (indexdy!=-1) {
-        apprenants[indexid].resultats[indexdy]=result
-        
-    }else
+
+        const result = {
+            jour: day, exercicesTermines: exercices,
+            totalExercices: 20, challengeTermine: challenge
+
+        }
+        let indexdy = misajourRusult(indexid, result)
+        if (indexdy != -1) {
+            apprenants[indexid].resultats[indexdy] = result
+
+        } else
             apprenants[indexid].resultats.push(result)
 
 
 
-        
-    }else
-        console.log( " Id Is not valider");
-        
-return indexid
-     
+
+    } else
+        console.log(" Id Is not valider");
+
+    return indexid
+
 
 }
 
 function FindByName(name) {
     name = prompt("Enter name You want to find : ")
 
-    name=normaliserNom(name)
+    name = normaliserNom(name)
     for (let i = 0; i < apprenants.length; i++) {
         if (apprenants[i].nomComplet.includes(name)) {
             return apprenants[i]
-            
 
-            
+
+
         }
-        
+
     }
-    
+
 }
- function serchebyId(id) {
+function serchebyId(id) {
     id = prompt("Enter Id ")
-    id= Number(id)
+    id = Number(id)
     for (let i = 0; i < apprenants.length; i++) {
-        if (apprenants[i].id==id) {
+        if (apprenants[i].id == id) {
             return apprenants[i]
-            
-        }
-        
-    }
-    
- }
-function calculerProgression(indexid) {
-    let totalexercices =20
-    let Exercicesterminés=0
-   let Exercicesproposés=0
-   let  Progression=0,CHallengesterminés=0,JournéesRenseignées=0
-    for (let i = 0; i < apprenants.length; i++) {
-        if (apprenants[i].id==indexid) {
-            Exercicesproposés=totalexercices*apprenants[i].resultats.length
-            for (let j = 0; j < apprenants[i].resultats.length; j++) {
-                JournéesRenseignées=apprenants[i].resultats.length
-                Exercicesterminés+=apprenants[i].resultats[j].exercicesTermines
-                if (apprenants[i].resultats[j].challengeTermine==true) {
-                    CHallengesterminés++
-                    
-                }
-                    Progression=(Exercicesterminés/Exercicesproposés)*100
 
-                
+        }
+
+    }
+
+}
+function calculerProgression(indexid) {
+    let totalexercices = 20
+    let Exercicesterminés = 0
+    let Exercicesproposés = 0
+    let Progression = 0, CHallengesterminés = 0, JournéesRenseignées = 0
+    for (let i = 0; i < apprenants.length; i++) {
+        if (apprenants[i].id == indexid) {
+            Exercicesproposés = totalexercices * apprenants[i].resultats.length
+            for (let j = 0; j < apprenants[i].resultats.length; j++) {
+                JournéesRenseignées = apprenants[i].resultats.length
+                Exercicesterminés += apprenants[i].resultats[j].exercicesTermines
+                if (apprenants[i].resultats[j].challengeTermine == true) {
+                    CHallengesterminés++
+
+                }
+                Progression = (Exercicesterminés / Exercicesproposés) * 100
+
+
             }
-                        return [
+            return [
                 totalexercices,
                 Exercicesterminés,
                 Exercicesproposés,
@@ -206,15 +206,74 @@ function calculerProgression(indexid) {
 
 
 
-            
+
         }
-        
-        
+
+
     }
-    
-return [totalexercices,Exercicesterminés,Exercicesproposés,Exercicesproposés]
-    
-} 
 
 
-console.log(calculerProgression(1));
+}
+
+function filtrerParNiveau(Choice) {
+    console.log("1 Solide :");
+    console.log("2 En progression :");
+    console.log("3 À renforcer :");
+
+    Choice = Number(prompt("  Enter Choice: "))
+    let Solide = [],Enprogression=[], Àrenforcer=[]
+
+    if (Choice == 1) {
+        for (let i = 0; i < apprenants.length; i++) {
+            let arr = calculerProgression(apprenants[i].id)
+            if (arr[3] >= 80) {
+                Solide.push({
+                    id: apprenants[i].id,
+                    nom: apprenants[i].nomComplet,
+                    ville: apprenants[i].ville,
+                    progress: arr[3]
+                })
+
+            }
+
+        }
+return Solide
+    } else if (Choice == 2) {
+        for (let i = 0; i < apprenants.length; i++) {
+            let arr = calculerProgression(apprenants[i].id)
+            if (arr[3] > 50 && arr[3] < 80) {
+                 Enprogression.push({
+                    id: apprenants[i].id,
+                    nom: apprenants[i].nomComplet,
+                    ville: apprenants[i].ville,
+                    progress: arr[3]
+                })
+
+
+
+            }
+
+        }
+        return Enprogression
+
+    } else
+        for (let i = 0; i < apprenants.length; i++) {
+            let arr = calculerProgression(apprenants[i].id)
+            if (arr[3] > 50 && arr[3] < 80) {
+                 Àrenforcer.push({
+                    id: apprenants[i].id,
+                    nom: apprenants[i].nomComplet,
+                    ville: apprenants[i].ville,
+                    progress: arr[3]
+                })
+
+
+            }
+            return Àrenforcer
+
+        }
+
+
+
+}
+console.table(filtrerParNiveau());
